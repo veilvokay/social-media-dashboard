b96dbdef814d9683ff8642beb31fe9c66873759d
Roman Yakovliev <romayakovlev@hotmail.com>
2021-12-12 23:33:48 +0200

    finished w/ theme changeing; depending on localStorage + prefer-color-scheme (css)

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,72 @@
-window.addEventListener('DOMContentLoaded', (e) => {
-    const body = document.body
-    const toggleWrapper = document.querySelector('.toggle__wrapper');
-    const inputs = document.querySelectorAll('.toggle__wrapper input');
-    const toggleDark = document.getElementById('dark');
-    const toggleLight = document.getElementById('light');
+const body = document.body
+const inputs = document.querySelectorAll('.toggle__wrapper input');
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
 
-    toggleWrapper.addEventListener('click', () => {
-        const selectedInput = Array.from(inputs).filter(el => el.checked)[0].id;
-        body.className = '';
-        body.classList.add(selectedInput);
-    });
-})
+const resetInputs = () => {
+    inputs.forEach(input => input.checked = false);
+}
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('change', e => {
+        const themeColor = e.target.id;
+        body.classList = themeColor;
+        localStorage.setItem('colorTheme', themeColor);
+    })
+}
+
+const getColorTheme = () => {
+    return localStorage.getItem('colorTheme');
+}
+
+const checkUserTheme = () => {
+    if (getColorTheme == null) {
+        resetInputs();
+        if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+            lightButton.checked = true;
+        }
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            darkButton.checked = true;
+        }
+    }
+}
+
+const checkThemeChange = () => {
+    window
+        .matchMedia("(prefers-color-scheme: light)")
+        .addEventListener('change', e => {
+            checkUserTheme();
+        })
+}
+
+const setDarkTheme = () => {
+    body.classList = 'dark';
+}
+
+const setLightTheme = () => {
+    body.classList = 'light';
+}
+
+const setColorTheme = () => {
+    const colorTheme = getColorTheme();
+    resetInputs();
+
+    switch (colorTheme) {
+        case 'dark':
+            setDarkTheme();
+            console.log('done');
+            darkButton.checked = true;
+            break;
+        case 'light':
+            setLightTheme();
+            lightButton.checked = true; 
+            break;
+        default:
+            break;
+    }
+}
+
+
+setColorTheme();
+checkThemeChange();
+
